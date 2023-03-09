81e482b0db85fc53f231ef7bcd4b9db1fb98d44d
rob.jordan054@gmail.com <rob.jordan054@gmail.com>
2023-03-08 19:46:45 -0500

    if else number

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace SelectionStatementExercise
+﻿using System;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            {
+
+                Console.WriteLine($"Try to guess my favorite number!!");
+                var userInput = int.Parse(Console.ReadLine());
+
+                if (userInput > favNumber)
+                {
+                    Console.WriteLine($"You've gone too far!");
+                }
+                else if (userInput < favNumber)
+                {
+                    Console.WriteLine($"Not high enough!");
+                }
+                else
+                {
+                    Console.WriteLine($"KA CHING!!");
+                }
+
+            }
         }
     }
 }
